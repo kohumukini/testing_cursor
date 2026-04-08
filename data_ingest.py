b308cf39c3f0e6d555bc36cac87b6ddc994483b0
Kealiiahonui Ohumukini <217384785+Drrrkkkkooo@users.noreply.github.com>
2026-04-08 15:27:13 -0700

    Testing w/ Google data function

---
+++ data_ingest.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+def get_google_data(): 
+    google_data = yf.download("GOOG", start = "2024-01-01", end = "2026-01-01")
+    google_df = google_data.reset_index()
+    google_df = google_df[['Date', 'Open', 'Close', 'Volume']]
+    return google_df
+
+print(get_google_data().head())
