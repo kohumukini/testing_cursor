3f9bbaebca4848fd643a65a5ae7627c20a62598e
Kealiiahonui Ohumukini <kealii.ohumukini@gmail.com>
2026-04-14 20:36:01 -0700

    Date change

--- data_ingest.py
+++ data_ingest.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 
 def get_stock_data(ticker): 
-    stock_data = yf.download(ticker, start = "2024-01-01", end = "2026-01-01")
+    stock_data = yf.download(ticker, start = "2024-01-01")
 
     if isinstance(stock_data.columns, pd.MultiIndex): 
         stock_data.columns = stock_data.columns.get_level_values(0)
