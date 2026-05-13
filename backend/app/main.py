f93d79d6e2ed00cd15a2561fcb927d1faf014e33
kohumukini <kealii.ohumukini@gmail.com>
2026-05-13 16:02:18 -0700

    Implemented data_backfill; Made changes and improvements

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.database import ENGINE, init_db
 from app.seed import seed_watchlist
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["http://localhost:5173"], 
+    allow_methods=["*"], 
+    allow_headers=["*"]
+)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
--- pipeline/pull.py
+++ pipeline/pull.py
@@ -10,6 +10,10 @@ def get_active_watchlist():
         return [t.ticker for t in tickers]
     
 def save_raw_data(ticker, data_frame): 
+    if data_frame.emtpy(): 
+        print(f"No data for {ticker}: Skipping... ")
+        return
+
     with SessionLocal() as session: 
         payload_raw = data_frame.to_json()
 
@@ -20,3 +24,19 @@ def save_raw_data(ticker, data_frame):
 
         session.add(new_entry)
         session.commit()
+        print(f"Saved {ticker} data")
+
+def data_backfill(): 
+    active_tickers = get_active_watchlist()
+
+    for t in active_tickers: 
+        with SessionLocal as session:
+            exists = session.query(BronzeStock).filter(BronzeStock.ticker == t).first()
+
+            if exists: 
+                ticker_data = yf.download(t, period="5y", interval="1d")
+            else: 
+                ticker_data = yf.download(t, period="1h", interval="1m")
+
+            save_raw_data(t, ticker_data)
+        
